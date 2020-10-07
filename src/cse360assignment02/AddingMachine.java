3e4652154d51f63df76b072de92f70a50c9a9f6b
nkintzle <72329907+nkintzle@users.noreply.github.com>
2020-10-06 23:13:48 -0700

    Update AddingMachine.java

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -1,26 +1,34 @@
 package cse360assignment02;
 
-public class AddingMachine {
+public class AddingMachine { // declare private variables
     private int total;
+    private String printTotal;
 
-    public AddingMachine () {
+    public AddingMachine () { // AddingMachine function initialises variables to 0
         total = 0;  // not needed - included for clarity
+        printTotal = "0";
     }
 
-    public int getTotal () {
-        return 0;
+    public int getTotal () { // getTotal returns total
+        return total;
     }
 
-    public void add (int value) {
+    public void add (int value) { // add adds value and appends it to previous printTotal value
+        total += value;
+        printTotal = printTotal + " + " + value;
     }
 
-    public void subtract (int value) {
+    public void subtract (int value) { // subtract function adds value and appends it to previous printTotal value
+        total -= value;
+        printTotal = printTotal + " - " + value;
     }
 
-    public String toString () {
-        return "";
+    public String toString () { // returns printTotal in concatenated string format
+        return printTotal.toString();
     }
 
-    public void clear() {
+    public void clear() { // clear function "resets memory"
+        total = 0;
+        printTotal = "0";
     }
 }
